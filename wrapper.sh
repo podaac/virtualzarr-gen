967169390458b367c207b158540a8ea1fb6b5d1c
mike-gangl <michael.e.gangl@jpl.nasa.gov>
2025-07-09 13:48:28 -0700

    added dates to generated files when provided. moved upload to wrapper script, out of notebook

--- wrapper.sh
+++ wrapper.sh
@@ -18,6 +18,6 @@ EOF
 
 papermill generate_cloud_optimized_store_https.ipynb output.ipynb --log-output  -p collection $COLLECTION -p loadable_coord_vars $LOADABLE_VARS -p start_date $startDate -p end_date $endDate -p bucket $OUTPUT_BUCKET
 
-# TODO move the copy of the outputfile from notebook to this wrapper.
-# TODO, need to allow aws sync for icechunk, parquet directories
-aws s3 cp output.ipynb s3://$OUTPUT_BUCKET/virtualcollection/$COLLECTION/output.ipynb
+# This call will upload the output.ipynb and then virtual collection (regardless of name) to the output location
+aws s3 sync . s3://$OUTPUT_BUCKET/virtualcollection/$COLLECTION/ --exclude "*" --include "*virtual.json" --include "*.ipynb"
+
