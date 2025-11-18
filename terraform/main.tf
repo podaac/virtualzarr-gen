30fd93e08d6e5f9a2821b9bb01cb3a629b05d508
Simon Liu <simon.liu@jpl.nasa.gov>
2025-11-18 09:57:43 -0800

    update ec2 instance type and noteboo

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,8 +50,8 @@ resource "aws_ecs_task_definition" "app" {
             "name": "cloud-optimization-generation",
             "image": "ghcr.io/podaac/virtualzarr-gen:main",
             "cpu": 0,
-            "memory": 65536,
-            "memoryReservation": 61440,
+            "memory": 249856,
+            "memoryReservation": 245760,
             "portMappings": [
                 {
                     "name": "workers",
@@ -231,7 +231,7 @@ resource "aws_launch_template" "app-lt" {
   }
   image_id = data.aws_ssm_parameter.ecs_amis.value
   vpc_security_group_ids = [data.aws_security_groups.vpc_default_sg.ids[0]]
-  instance_type = "c5.9xlarge"
+  instance_type = "r5.8xlarge"
   user_data = base64encode(templatefile("scripts/ecs.sh", { ecs_cluster = aws_ecs_cluster.default.name}))
   update_default_version = true
 }
