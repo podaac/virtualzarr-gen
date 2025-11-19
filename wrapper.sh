fcec7fdd3dc5da624ee3bbc8c5d9dcbc29f121d0
Simon Liu <simon.liu@jpl.nasa.gov>
2025-11-19 13:47:42 -0800

    update shell script

--- wrapper.sh
+++ wrapper.sh
@@ -1,29 +1,12 @@
 #!/bin/bash
 # Environment Variables must be passed into the docker run command
 
-[[ -z "${START_DATE}" ]] && startDate='None' || startDate="${START_DATE}"
-[[ -z "${END_DATE}" ]] && endDate='None' || endDate="${END_DATE}"
-
-#need to update this 
-export edl_username=`aws ssm get-parameter --with-decryption --name $SSM_EDL_USERNAME | jq .Parameter.Value --raw-output`
-export edl_password=`aws ssm get-parameter --with-decryption --name $SSM_EDL_PASSWORD | jq .Parameter.Value --raw-output`
-
-cat > ~/.netrc <<EOF
-machine urs.earthdata.nasa.gov
-  login $edl_username
-  password $edl_password
-EOF
-
-# papermill generate_cloud_optimized_store_https.ipynb output.ipynb --log-output  -p collection $COLLECTION -p loadable_coord_vars $LOADABLE_VARS -p start_date $startDate -p end_date $endDate -p bucket $OUTPUT_BUCKET
-python3 generate_vds_clean.py --collection $COLLECTION --loadable-coord-vars $LOADABLE_VARS --start-#!/bin/bash
-# Environment Variables must be passed into the docker run command
-
 [[ -z "${START_DATE}" ]] && startDate='' || startDate="${START_DATE}"
 [[ -z "${END_DATE}" ]] && endDate='' || endDate="${END_DATE}"
 
-#need to update this 
-export edl_username=`aws ssm get-parameter --with-decryption --name $SSM_EDL_USERNAME | jq .Parameter.Value --raw-output`
-export edl_password=`aws ssm get-parameter --with-decryption --name $SSM_EDL_PASSWORD | jq .Parameter.Value --raw-output`
+# Retrieve EDL credentials from AWS SSM
+export edl_username=$(aws ssm get-parameter --with-decryption --name $SSM_EDL_USERNAME | jq .Parameter.Value --raw-output)
+export edl_password=$(aws ssm get-parameter --with-decryption --name $SSM_EDL_PASSWORD | jq .Parameter.Value --raw-output)
 
 cat > ~/.netrc <<EOF
 machine urs.earthdata.nasa.gov
@@ -41,8 +24,5 @@ fi
 
 eval $cmd
 
-# This call will upload the output.ipynb and then virtual collection (regardless of name) to the output location
-aws s3 sync . s3://$OUTPUT_BUCKET/virtualcollection/$COLLECTION/ --exclude "*" --include "*virtual_https.json"date $startDate --end-date $endDate
-
-# This call will upload the output.ipynb and then virtual collection (regardless of name) to the output location
+# Upload output files to S3
 aws s3 sync . s3://$OUTPUT_BUCKET/virtualcollection/$COLLECTION/ --exclude "*" --include "*virtual_https.json" --include "output.ipynb" --exclude ".ipynb_checkpoints"
