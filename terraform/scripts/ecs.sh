b454fbd7090c255ce157ab081fd78f8eb6453d7d
Simon Liu <simon.liu@jpl.nasa.gov>
2025-12-02 18:01:30 -0800

    /deploy sit

--- terraform/scripts/ecs.sh
+++ terraform/scripts/ecs.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-
-# 1. Put placeholder cluster so ECS agent does nothing
-echo "ECS_CLUSTER=__DELAY_CLUSTER__" > /etc/ecs/ecs.config
-
-# 2. Wait your delay (3 minutes in your case)
-sleep 180
-
-# 3. Replace with real cluster name
 echo "ECS_CLUSTER=${ecs_cluster}" > /etc/ecs/ecs.config
 
-# 4. Restart ECS agent cleanly
-sudo systemctl restart ecs
+sudo systemctl stop ecs
+
+sleep 180
+
+sudo systemctl start ecs
